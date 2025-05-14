0b1c40e36809ff964d8934cfdab94a60d3a8ef62
PajovicVanja <vanja.pajovic@student.um.si>
2025-05-14 11:35:45 +0200

    sql change

--- payment-service/creatingdb.sql
+++ payment-service/creatingdb.sql
@@ -5,6 +5,7 @@ USE paymentdb;
 CREATE TABLE IF NOT EXISTS payments (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT NOT NULL,
+    barber_id BIGINT NOT NULL,
     reservation_id VARCHAR(50) NOT NULL,
     amount DECIMAL(10,2) NOT NULL,
     status VARCHAR(50) NOT NULL,
@@ -12,7 +13,7 @@ CREATE TABLE IF NOT EXISTS payments (
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO payments (user_id, reservation_id, amount, status, payment_method)
+INSERT INTO payments (user_id, reservation_id, barber_id, amount, status, payment_method)
 VALUES
-(1, 101, 49.99, 'success', 'credit_card'),
-(2, 102, 75.00, 'pending', 'paypal');
+(1, '101', 10, 49.99, 'success', 'credit_card'),
+(2, '102', 12, 75.00, 'pending', 'paypal');
